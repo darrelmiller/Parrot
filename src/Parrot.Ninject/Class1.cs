1ab02247f0bfb073c27ad4858b8907373f5bf4f7
Ben Dornis <ben@buildstarted.com>
2012-08-06 20:41:42 -0700

    boilerplate code for ninject dependency resolver

---
+++ src/Parrot.Ninject/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+
+namespace Parrot.Ninject
+{
+    public class NinjectParrotDependencyResolver : Infrastructure.DependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectParrotDependencyResolver(IKernel kernel)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException("kernel");
+            }
+
+            _kernel = kernel;
+        }
+
+        public override T Get<T>()
+        {
+            return _kernel.TryGet<T>() ?? base.Get<T>();
+        }
+
+        public override object Get(Type type)
+        {
+            return _kernel.TryGet(type) ?? base.Get(type);
+        }
+    }
+}
